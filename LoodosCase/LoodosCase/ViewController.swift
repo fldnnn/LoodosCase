bbb1b06b11f42da0e8c9b866d736796519a141ba
fulden onan <>
2024-11-09 15:26:22 +0300

    first commit

---
+++ LoodosCase/LoodosCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoodosCase
+//
+//  Created by fulden onan on 9.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
